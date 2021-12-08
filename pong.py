d8e309234cd4018f2c8d85bc32423bd38d47d1af
MatheusOde <matheusode@gmail.com>
2021-12-08 15:17:14 -0300

    added score

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 # window setup part
 
@@ -7,6 +8,16 @@ window.title = "Pong for AI"
 window.bgcolor('#09001C')
 window.setup(width=600, height=480)
 window.tracer(0)
+
+# score
+player = 0
+computer = 0
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("#C9C7B5")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,170)
 
 # paddle area
 
@@ -36,14 +47,21 @@ ball.penup()
 ball.goto(0,0)
 ball.speedInXAxis = 0.1
 ball.speedInYAxis = 0.1
+
 # player movement
 
 def paddleUp():
+    if (playerPaddle.ycor() >= 215):
+        return
+
     y = playerPaddle.ycor()
     y += 10
     playerPaddle.sety(y)
 
 def paddleDown():
+    if (playerPaddle.ycor() <= -215):
+        return
+
     y = playerPaddle.ycor()
     y -= 10
     playerPaddle.sety(y)
@@ -52,11 +70,24 @@ def resetGame():
     ball.goto(0,0)
     playerPaddle.goto(-250,0)
     machinePaddle.goto(250,0)
+    computer = 0
+    pen.clear()
 
 window.listen()
 window.onkeypress(paddleUp, "w")
 window.onkeypress(paddleDown, "s")
 window.onkeypress(resetGame, "r")
+
+#invincible bot movement
+def followBall():
+    if ball.xcor() >= 0 and ball.speedInXAxis > 0:
+        y = machinePaddle.ycor()
+        if ball.ycor() < y:
+            y -= 10
+            machinePaddle.sety(y)
+        if ball.ycor() > y:
+            y += 10
+            machinePaddle.sety(y)
 
 # main game area
 
@@ -65,7 +96,7 @@ while(True):
 # ball movement
     ball.setx(ball.xcor() + ball.speedInXAxis)
     ball.sety(ball.ycor() + ball.speedInYAxis)
-    
+    followBall()
     if ball.ycor() >= 240 or ball.ycor() <= -240:
         ball.speedInYAxis = - ball.speedInYAxis
     
@@ -79,3 +110,10 @@ while(True):
 # points rules
     if ball.xcor() <= -300:
         ball.goto(0,0)
+        computer += 1
+        pen.clear()
+        pen.write("Player: {}    Computer: {}".format(player, computer) , align="center", font=("Times New Roman", 16, "normal"))
+
+        
+
+
