a3cba7b0bd0318a7de5e6a1db3e2945a45090543
MatheusOde <matheusode@gmail.com>
2021-12-09 16:45:17 -0300

    added some coments

--- AI.py
+++ AI.py
@@ -1,13 +1,20 @@
+# to use the screen as an array of pixels
 import numpy as np
+# to grab the screen
 import pyscreenshot as ImageGrab
+# to scan the screen color
 import cv2
+
+#to get inputs and post outputs on to the game
 import keyboard
 import mouse
+
 import time
 
 screen = np.array(ImageGrab.grab(bbox=gameCoords))
 screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
+# AI loop
 for i in range(1000):
     startTime = time.time()
     for y in range(len(screen)):
